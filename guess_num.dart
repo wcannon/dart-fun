8922dcefaa4f34f53034f758dc0853b234b5f3ff
William Cannon <william.cannon@gmail.com>
2023-05-25 07:04:49 -0500

    Do while loop
    Now continues asking for guess with a do while loop

--- guess_num.dart
+++ guess_num.dart
@@ -2,17 +2,20 @@
 import 'dart:io';
 
 void main() {
-  // Asking for favourite number
-  print("Enter your favourite number:");
-
-  // Scanning number
-  int? n = int.parse(stdin.readLineSync()!);
-  // Here ? and ! are for null safety
-
   var secret = 7;
-  if (secret == n) {
-    print('You guessed the secret number');
-  } else {
-    print('Wrong number');
-  }
+  int n = 0;
+  do {
+    // Asking for favourite number
+    print("Guess the secret number :");
+
+    // Scanning number
+    int? n = int.parse(stdin.readLineSync()!);
+    // Here ? and ! are for null safety
+    if (secret == n) {
+      print('You guessed the secret number');
+      break;
+    } else {
+      print('Wrong number');
+    }
+  } while (secret != n);
 }
